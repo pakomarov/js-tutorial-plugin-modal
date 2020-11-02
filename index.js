d72caf311b432e511836174d63ae09fab26bddf7
Pavel Komarov <komarov.pa.it@gmail.com>
2020-11-03 01:44:09 +0300

    Adds functionality to modal plugin

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-const myModal = $.modal();
+const fruits = [
+  {id: 1, title: 'Яблоки', price: 20,},
+  {id: 2, title: 'Апельсины', price: 30,},
+  {id: 3, title: 'Манго', price: 40,},
+]
+
+const myModal = $.modal({
+  title: 'My Modal',
+  closable: true,
+  content: `
+    <h4>Modal is working</h4>
+    <p>Lorem ipsum dolor sit.</p>
+  `,
+  width: '400px',
+  footerButtons: [
+    {text: 'Ok', type: 'primary', handler() {
+      console.log('Primary btn was clicked');
+      myModal.close();
+    }},
+    {text: 'Cancel', type: 'danger', handler() {
+      console.log('Cancel btn was clicked');
+      myModel.close();
+    }},
+  ],
+  onOpen() {
+    console.log("I'm open!");
+  },
+  beforeClose() {
+    console.log("I'm about to close!");
+  },
+  onClose() {
+    console.log("I'm closed!");
+  },
+});
--- plugins/modal.js
+++ plugins/modal.js
@@ -1,45 +1,85 @@
 (function () {
   const ANIMATION_SPEED = 200;
+  const WINDOW_DEFAULT_WIDTH = '600px';
 
+  const noop = () => {};
 
-  const _createModal = function (options) {
+  const _createModalFooter = (buttons = []) => {
+    if (buttons.length === 0) {
+      return document.createElement('div');
+    }
+
+    const containerElement = document.createElement('div');
+    containerElement.classList.add('modal-footer');
+
+    buttons.forEach(btn => {
+      const btnElement = document.createElement('button');
+      btnElement.textContent = btn.text;
+      btnElement.classList.add('btn');
+      btnElement.classList.add(`btn-${btn.type || 'secondary'}`);
+      btnElement.onclick = btn.handler || noop;
+
+      containerElement.appendChild(btnElement);
+    });
+
+    return containerElement;
+  };
+
+  const _createModal = (options) => {
     const modalElement = document.createElement('div');
     modalElement.classList.add('my-modal');
     modalElement.insertAdjacentHTML('afterbegin', `
-      <div class="modal-overlay">
-        <div class="modal-window">
+      <div class="modal-overlay" data-close="true">
+        <div class="modal-window" style="width: ${options.width || WINDOW_DEFAULT_WIDTH}">
           <div class="modal-header">
-            <span class="modal-title">Modal title</span>
-            <span class="modal-close">&times;</span>
+            <span class="modal-title">${options.title || 'Окно'}</span>
+            ${options.closable ? `<span class="modal-close" data-close="true">&times;</span>` : ''}
           </div>
-          <div class="modal-body">
-            <p>Lorem ipsum dolor sit.</p>
-            <p>Lorem ipsum dolor sit.</p>
-          </div>
-          <div class="modal-footer">
-            <button>Ok</button>
-            <button>Cancel</button>
+          <div class="modal-body" data-content>
+            ${options.content || ''}
           </div>
         </div>
       </div>
     `);
+
+    const footerElement = _createModalFooter(options.footerButtons);
+    modalElement.querySelector('.modal-body').after(footerElement);
     return modalElement;
   };
 
-  const _renderModel = function (modalElement) {
+  const _renderModal = (modalElement) => {
     document.body.appendChild(modalElement);
   };
 
-  const modal = function (options) {
+  const modal = (options) => {
     const modalElement = _createModal(options);
-    _renderModel(modalElement);
+    _renderModal(modalElement);
     let isClosing = false;
+    let isDestroyed = false;
 
-    return {
+    const modal = {
       open() {
+        if (isDestroyed) {
+          console.log('Modal is destroyed');
+          return;
+        }
+
         !isClosing && modalElement.classList.add('opened');
+
+        if (typeof options.onOpen === "function") {
+          options.onOpen();
+        }
       },
       close() {
+        if (isDestroyed) {
+          console.log('Modal is destroyed');
+          return;
+        }
+
+        if (typeof options.beforeClose === "function") {
+          options.beforeClose();
+        }
+
         isClosing = true;
         modalElement.classList.remove('opened');
         modalElement.classList.add('hiding')
@@ -47,9 +87,35 @@
           modalElement.classList.remove('hiding');
           isClosing = false;
         }, ANIMATION_SPEED);
+
+        if (typeof options.onClose === "function") {
+          options.onClose();
+        }
       },
-      destroy() {},
+      destroy() {
+        if (isDestroyed) {
+          console.log('Modal is destroyed');
+          return;
+        }
+
+        modalElement.parentNode.removeChild(modalElement);
+        modalElement.removeEventListener('clicl', modalClickHandler);
+        isDestroyed = true;
+      },
+      setContent(html) {
+        modalElement.querySelector('[data-content]').innerHTML = html;
+      }
     };
+
+    const modalClickHandler = (evt) => {
+      if (evt.target.dataset.close) {
+        modal.close();
+      }
+    };
+
+    modalElement.addEventListener('click', modalClickHandler);
+
+    return modal;
   };
 
   $.modal = modal;
